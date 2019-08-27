920e30f7f366f2ed30a392fb020190f990600bcf
FREDERICK ARTICUNO <mckiyoshi@gmail.com>
2019-08-27 17:20:03 -0400

    added goblin -currently malfunctioning due to poor logic

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function buildMain() {
     for (i = 0; i < 12; i++) {
         mapCell = $("<div>");
         xy = [i,j];
-        mapCell.addClass("mapcell col-md-1")
+        mapCell.addClass("mapcell col-md-1");
         mapCell.attr("coordinates",xy[i,j]);
         mapCell.attr("float","left");
         mapCell.css("background-image",'url(https://tinyurl.com/y5q7gcap)');
@@ -27,8 +27,8 @@ function buildMain() {
         mapCell.text(xy);
         $("#mainMap"+n).append(mapCell);
         
-    }
-    console.log(mapCell)
+    };
+    console.log(mapCell);
     if (j < 11 && i == 12) {
         j++;
         n++;
@@ -38,6 +38,27 @@ function buildMain() {
         $("#cont").append(mapRow);
         i = 0;
         buildMain();
-    }
-}
+    };
+};
 
+//  Currently Unresponsive, image showe in 11,11 only
+    //  need to get current location by coordinates[i,j] and move with keypress
+i = 0;
+j = 0;
+pxy = [0,0];
+document.onkeyup = function(event) {
+    
+    if (event.key == "w") {
+        $("coordinates",xy[i,j+=1])
+    }
+    if (event.key == "a") {
+        $("coordinates",xy[i-=1,j])
+    }
+    if (event.key == "s") {
+        $("coordinates",xy[i,j-=1])
+    }
+    if (event.key == "d") {
+        $("coordinates",xy[i+=1,j])
+    }
+    mapCell.html('<img src="./images/goblin.png"/>')
+}
